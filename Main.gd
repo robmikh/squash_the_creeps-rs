b9d025a1c84e7c3f80a19d9c075dc1027a7f6966
robmikh <rob.mikh@outlook.com>
2021-06-13 21:20:32 -0700

    use GDScript

--- Main.gd
+++ Main.gd
@@ -9,14 +9,9 @@ func _ready():
 func on_mob_timer_timeout():
 	# Create a Mob instance and add it to the scene.
 	var mob = mob_scene.instance()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	mob_spawn_location.unit_offset = 0.5 #randf()
-
-	#var player_position = $Player.transform.origin
-	var player_position = Vector3(-4.7574863, 0.8057514, -6.1575346)
+	var spawner_location = $Spawner.transform.origin
+	var player_position = $ShootAt.transform.origin
 
 	add_child(mob)
-	mob.initialize(mob_spawn_location.translation, player_position)
+	mob.initialize(spawner_location, player_position)
 
--- Mob.gd
+++ Mob.gd
@@ -15,12 +15,8 @@ func _physics_process(_delta):
 func initialize(start_position, player_position):
 	translation = start_position
 	look_at(player_position, Vector3.UP)
-	#rotate_y(rand_range(-PI / 4, PI / 4))
 
-	var random_speed = rand_range(min_speed, max_speed)
-	# We calculate a forward velocity first, which represents the speed.
-	velocity = Vector3.FORWARD * random_speed
-	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
+	velocity = Vector3.FORWARD * max_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 
